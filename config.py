2ef8324eede4e8793ce08aa4a7d408a3830e929b
ZEUSxCODES <127749230+ZEUSxCODES@users.noreply.github.com>
2024-03-19 16:28:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -94,9 +94,9 @@ class Var(object):
     FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
     HAS_SSL=bool(getenv('HAS_SSL',False))
     if HAS_SSL:
-        URL = "ms-file-store-69f7f9eb5568.herokuapp.com"
+        URL = "https://ms-file-store-69f7f9eb5568.herokuapp.com"
     else:
-        URL = "ms-file-store-69f7f9eb5568.herokuapp.com"
+        URL = "https://ms-file-store-69f7f9eb5568.herokuapp.com"
 
 
 
